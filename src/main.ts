31025d8aeac90393954aebcfe75f8878069d5bda
chadlada <chadlada@gmail.com>
2022-03-03 13:28:50 -0500

    'Creating Variables' part done

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import './style.css'
 
+// ------------------------------Creating Variables------------------------------
+
 const numberOfCupsOfCoffee = 3
 const fullName = 'Chad Lada'
 
 console.log(`${fullName} ${numberOfCupsOfCoffee}`)
+
+const myObject = {
+  fullName: fullName,
+  luckyNumber: 23,
+  favoriteMovies: ['Idiocracy', 'The Shining', 'Pokemon'],
+}
+console.table(myObject)
+
+// ------------------------------Getting Input From User------------------------------
+
+// ------------------------------Converting String Input Into Numbers------------------------------
+
+// ------------------------------Doing Math------------------------------
